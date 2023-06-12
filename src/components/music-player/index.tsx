95698ac30ad9c428cb7b853b7841f6ccb4611c9c
na-go <kn.mbk201.mynk@gmail.com>
2023-06-12 18:23:53 +0900

    アイコンを追加

--- src/components/music-player/index.tsx
+++ src/components/music-player/index.tsx
@@ -1,26 +1,49 @@
 import { useCallback } from "react";
 
+import repeatOff from "@assets/icons/repeat-off.svg";
+import repeatOnOnce from "@assets/icons/repeat-on-once.svg";
+import volumeDown from "@assets/icons/volume-down.svg";
+import volumeUp from "@assets/icons/volume-up.svg";
 import { useMusicPlayer } from "@react/player";
 import { translateNumberToDate } from "@utils/translate-number-to-date";
+
 
 import * as styles from "./styles.css";
 
 export const MusicPlayer = () => {
-  const { currentTrack, currentTime, currentVolume, isPlaying, currentTrackInfo, play, pause, chooseTrack, seek, seekMouseDown, seekMouseUp, volume } = useMusicPlayer();
+  const {
+    currentTrack,
+    currentTime,
+    currentVolume,
+    isPlaying,
+    isRepeat,
+    currentTrackInfo,
+    play,
+    pause,
+    setTrack,
+    seek,
+    seekMouseDown,
+    seekMouseUp,
+    volume,
+    toggleRepeat: repeat,
+  } = useMusicPlayer();
   const handleFileChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const file = event.target.files?.item(0);
       if (!file) return;
-      chooseTrack(file);
+      setTrack(file);
     },
-    [chooseTrack]
+    [setTrack]
   );
 
-  const handleSeek = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    const seekTo = parseInt(event.target.value, 10);
-    if (Number.isNaN(seekTo)) return;
-    seek(seekTo);
-  }, [seek]);
+  const handleSeek = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const seekTo = parseInt(event.target.value, 10);
+      if (Number.isNaN(seekTo)) return;
+      seek(seekTo);
+    },
+    [seek]
+  );
 
   const handleSeekMouseUp = useCallback(async () => {
     await seekMouseUp();
@@ -30,11 +53,14 @@ export const MusicPlayer = () => {
     seekMouseDown();
   }, [seekMouseDown]);
 
-  const handleVolumeChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    const volumeTo = parseInt(event.target.value, 10);
-    if (Number.isNaN(volumeTo)) return;
-    volume(volumeTo / 100);
-  }, [volume]);
+  const handleVolumeChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const volumeTo = parseInt(event.target.value, 10);
+      if (Number.isNaN(volumeTo)) return;
+      volume(volumeTo / 100);
+    },
+    [volume]
+  );
 
   return (
     <div className={styles.playerContainer}>
@@ -45,25 +71,21 @@ export const MusicPlayer = () => {
       <div id="music-info" className={styles.musicInfo}>
         <span>{currentTrackInfo.title}</span>
       </div>
-      <div id="seek-bar-box" className={styles.seekBarBox}>
+      <div id="seek-bar-box" className={styles.seekBarContainer}>
         <div id="current-time" className={styles.musicInfo}>
-          <span>
-            {translateNumberToDate(currentTime)}
-          </span>
+          <span>{translateNumberToDate(currentTime)}</span>
         </div>
         <input
           type="range"
           min={0}
           max={currentTrackInfo.duration}
-          value={currentTime ?? 0}
+          value={currentTime}
           className={styles.seekBar}
           onChange={handleSeek}
           onMouseDown={handleSeekMouseDown}
           onMouseUp={handleSeekMouseUp}
         />
-        <span className={styles.duration}>
-          {translateNumberToDate(currentTrackInfo.duration)}
-        </span>
+        <span className={styles.duration}>{translateNumberToDate(currentTrackInfo.duration)}</span>
       </div>
       {currentTrack ? (
         <button onClick={isPlaying ? pause : play} className={styles.playPauseButton}>
@@ -72,26 +94,26 @@ export const MusicPlayer = () => {
       ) : (
         <p>No track selected</p>
       )}
-      {/* TODO: assets/icons/volume-down.svgとassets/icons/volume-up.svgでinputを挟む */}
       <div className={styles.volumeContainer}>
-        <img
-          className={styles.volumeIcon}
-          src='src/assets/icons/volume-down.png'
-          alt="volume-down"
-        />
+        <img className={styles.volumeIcon} src={volumeDown} alt="volume-down" />
         <input
           type="range"
           min={0}
           max={100}
-          value={currentVolume*100}
+          value={currentVolume * 100}
           className={styles.volumeBar}
           onChange={handleVolumeChange}
         />
-        <img
-          className={styles.volumeIcon}
-          src='src/assets/icons/volume-up.png'
-          alt="volume-down"
-        />
+        <img className={styles.volumeIcon} src={volumeUp} alt="volume-down" />
+      </div>
+      <div className={styles.optionContainer}>
+        <button onClick={repeat} className={styles.repeatButton}>
+          {isRepeat ? (
+            <img className={styles.repeatIcon} src={repeatOnOnce} alt="repeat" />
+          ) : (
+            <img className={styles.repeatIcon} src={repeatOff} alt="repeat" />
+          )}
+        </button>
       </div>
     </div>
   );
--- src/components/music-player/styles.css.ts
+++ src/components/music-player/styles.css.ts
@@ -1,118 +1,143 @@
-import { style } from '@vanilla-extract/css';
+import { style } from "@vanilla-extract/css";
 
 export const playerContainer = style({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: '#F8F8F8',
-  color: '#383838',
-  padding: '20px',
-  borderRadius: '20px',
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "#F8F8F8",
+  color: "#383838",
+  padding: "20px",
+  borderRadius: "20px",
   fontFamily: '"Comic Sans MS", cursive, sans-serif',
 });
 
 export const fileInputLabel = style({
-  marginBottom: '10px',
-  backgroundColor: '#FFB6C1',
-  color: '#FFFFFF',
-  padding: '10px',
-  borderRadius: '10px',
-  cursor: 'pointer',
-  ':hover': {
-    backgroundColor: '#FF69B4',
+  marginBottom: "10px",
+  backgroundColor: "#FFB6C1",
+  color: "#FFFFFF",
+  padding: "10px",
+  borderRadius: "10px",
+  cursor: "pointer",
+  ":hover": {
+    backgroundColor: "#FF69B4",
   },
 });
 
 export const fileInput = style({
-  display: 'none',
+  display: "none",
 });
 
 export const musicInfo = style({
-  marginTop: '10px',
-  marginBottom: '10px',
-  fontWeight: 'bold',
+  marginTop: "10px",
+  marginBottom: "10px",
+  fontWeight: "bold",
+  marginRight: "10px",
 });
 
-export const seekBarBox = style({
-  display: 'flex',
-  alignItems: 'center',
-  width: '100%',
-  marginBottom: '20px',
-  justifyContent: 'space-between',
+export const seekBarContainer = style({
+  display: "flex",
+  alignItems: "center",
+  width: "100%",
+  marginBottom: "20px",
+  justifyContent: "space-between",
 });
 
 export const seekBar = style({
-  appearance: 'none',
-  width: '90%',
-  height: '5px',
-  borderRadius: '5px',
-  backgroundColor: '#c0c0c0',
-  outline: 'none',
-  transition: 'opacity .2s',
-  cursor: 'pointer',
-  '::-webkit-slider-thumb': {
-    appearance: 'none',
-    width: '20px',
-    height: '20px',
-    borderRadius: '50%',
-    backgroundColor: '#FF69B4',
-    cursor: 'pointer',
+  appearance: "none",
+  width: "90%",
+  height: "5px",
+  borderRadius: "5px",
+  backgroundColor: "#c0c0c0",
+  outline: "none",
+  transition: "opacity .2s",
+  cursor: "pointer",
+  "::-webkit-slider-thumb": {
+    appearance: "none",
+    width: "20px",
+    height: "20px",
+    borderRadius: "50%",
+    backgroundColor: "#FF69B4",
+    cursor: "pointer",
   },
 });
 
 export const duration = style({
-  marginLeft: '10px',
-  marginTop: '10px',
-  marginBottom: '10px',
-  fontWeight: 'bold',
+  marginLeft: "10px",
+  marginTop: "10px",
+  marginBottom: "10px",
+  fontWeight: "bold",
 });
 
 export const playPauseButton = style({
-  padding: '10px 20px',
-  backgroundColor: '#70a1ff',
-  color: 'white',
-  borderRadius: '20px',
-  border: 'none',
-  cursor: 'pointer',
-  ':hover': {
-    backgroundColor: '#3498db',
+  padding: "10px 20px",
+  backgroundColor: "#70a1ff",
+  color: "white",
+  borderRadius: "20px",
+  border: "none",
+  cursor: "pointer",
+  ":hover": {
+    backgroundColor: "#3498db",
   },
 });
 
 export const volumeContainer = style({
-  display: 'flex',
-  alignItems: 'center',
-  width: '100%',
-  marginBottom: '20px',
-  justifyContent: 'space-between',
+  display: "flex",
+  alignItems: "center",
+  width: "100%",
+  marginBottom: "20px",
+  justifyContent: "space-between",
 });
 
 export const volumeIcon = style({
-  width: '20px',
-  height: '20px',
-  marginLeft: '10px',
-  marginRight: '10px',
-  marginTop: '20px',
-  cursor: 'pointer',
+  width: "20px",
+  height: "20px",
+  marginLeft: "10px",
+  marginRight: "10px",
+  marginTop: "20px",
+  cursor: "pointer",
 });
 
 export const volumeBar = style({
-  appearance: 'none',
-  width: '100%',
-  height: '5px',
-  marginTop: '20px',
-  borderRadius: '5px',
-  backgroundColor: '#D0D0D0',
-  outline: 'none',
-  transition: 'opacity .2s',
-  cursor: 'pointer',
-  '::-webkit-slider-thumb': {
-    appearance: 'none',
-    width: '20px',
-    height: '20px',
-    borderRadius: '50%',
-    backgroundColor: '#FF69B4',
-    cursor: 'pointer',
+  appearance: "none",
+  width: "100%",
+  height: "5px",
+  marginTop: "20px",
+  borderRadius: "5px",
+  backgroundColor: "#D0D0D0",
+  outline: "none",
+  transition: "opacity .2s",
+  cursor: "pointer",
+  "::-webkit-slider-thumb": {
+    appearance: "none",
+    width: "20px",
+    height: "20px",
+    borderRadius: "50%",
+    backgroundColor: "#FF69B4",
+    cursor: "pointer",
   },
+});
+
+export const optionContainer = style({
+  display: "flex",
+  alignItems: "center",
+  width: "100%",
+  marginBottom: "20px",
+  justifyContent: "center",
+});
+
+export const repeatButton = style({
+  backgroundColor: "#F8F8F8",
+  color: "white",
+  border: "none",
+  cursor: "pointer",
+});
+
+export const repeatIcon = style({
+  width: "20px",
+  height: "20px",
+  marginLeft: "10px",
+  marginRight: "10px",
+  marginTop: "20px",
+  cursor: "pointer",
 });
